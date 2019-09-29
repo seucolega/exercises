5bf3133db8fbc04d16b9f914872eee49b92e3f42
Gustavo Sousa <gustavosousa@jeporu.com>
2019-09-29 01:15:53 -0300

    Resolution of the "ISBN Verifier" exercise

---
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,9 @@
+from re import search
+
+
+def is_valid(isbn):
+    match = search(r'^(\d{9})(\d|X)$', isbn.replace('-', ''))
+    if not match:
+        return False
+    isbn = list(match.string)
+    return sum([10 if digit == 'X' else int(digit) * index for digit, index in zip(isbn, range(10, 0, -1))]) % 11 == 0

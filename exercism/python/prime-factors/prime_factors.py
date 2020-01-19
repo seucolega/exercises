cad3ec3260d1885fae01e2fd1f72f91f242a0840
Gustavo Sousa <gustavosousa@jeporu.com>
2020-01-18 23:15:55 -0300

    Solution for Prime Factors

---
+++ exercism/python/prime-factors/prime_factors.py
@@ -0,0 +1,9 @@
+def factors(value: int) -> list:
+    result = []
+    divisor = 2
+    while value > 1:
+        while value % divisor == 0:
+            result.append(divisor)
+            value /= divisor
+        divisor += 1
+    return result

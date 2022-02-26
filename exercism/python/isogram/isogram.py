9455796fe93ba356de7b10518b7c7cbc7e6ef041
Gustavo Sousa <ola@gustavosousa.me>
2022-02-26 12:59:09 -0300

    Updating Isogram from Exercism, and improving my code

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -3,4 +3,9 @@ from re import sub
 
 def is_isogram(string: str) -> bool:
     string = sub('[ -]', '', string).lower()
-    return [0 for letter, index in zip(string, range(len(string))) if letter in string[index + 1:]] == []
+
+    for index in range(len(string)):
+        if string[index] in string[index + 1:]:
+            return False
+
+    return True

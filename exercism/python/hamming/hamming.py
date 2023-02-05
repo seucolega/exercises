0465f04cde9d8fb378806e52cefeb7e7ddcf4eb8
Gustavo Sousa <ola@gustavosousa.me>
2023-02-05 12:20:36 -0300

    Updating Python Hamming exercise

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
-        raise ValueError("A very specific bad thing happened.")
-    return len([1 for compare_a, compare_b in zip(strand_a, strand_b) if compare_a != compare_b])
+        raise ValueError("Strands must be of equal length.")
+    return len([1 for base_a, base_b in zip(strand_a, strand_b) if base_a != base_b])
